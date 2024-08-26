9d7b4035b9b54acb734881c8995aba3dac66b3a6
SubjugatorofCSS Rose <george.rose@students.codeinstitute.net>
2024-08-26 14:44:20 +0000

    superuser created

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgres://ubnib0huau7:eZXgxN2QpbWV@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/flick_stage_dingy_710907")
--- switch_sad/models.py
+++ switch_sad/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Appointment(models.Model):
+ """
+  Stores or adds information about an therapy appointment enquiry.
+ """
+
+patient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        )
+
+diagnosis = models.CharField(
+        max_length=500
+        )
+
+created_on = models.DateTimeField(
+        auto_now_add=True
+    )
+
+edited_on = models.DateTimeField(
+        auto_now=True
+    )
+
+class Meta:
+        verbose_name = "Appointment"
+        verbose_name_plural = "Appointmentss"
+        ordering = ["-created_on"]
+
+def __str__(self):
+        return f"Appointment request of {self.patient} with {self.diagnosis}"    
